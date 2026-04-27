625461924a2501c11ae55edd6ea51d18ba28a3c2
paulveloz28 <paulveloz28@gmail.com>
2026-04-27 16:01:24 -0500

    feat: implement logic to generate multiplication tables dynamically

--- multiplicar.js
+++ multiplicar.js
@@ -1,3 +1,15 @@
 function generarTablas(){
-    
+    let contenedor=document.getElementById("contenedor-tabla");
+
+    let contenido = "";
+
+    for (let i = 1; i <= 10; i++) {
+        
+        contenido = contenido + `
+            <tr>
+                <td>5 x ${i}</td>
+                <td class="glow-cell">${5 * i}</td>
+            </tr>`;    
+    }
+    contenedor.innerHTML=contenido;
 }
