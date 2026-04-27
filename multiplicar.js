52aa37f4206e4e12fb0d461ebfe60fdae65e4e55
paulveloz28 <paulveloz28@gmail.com>
2026-04-27 16:18:57 -0500

    feat: read the value entered in the text box

--- multiplicar.js
+++ multiplicar.js
@@ -1,15 +1,21 @@
-function generarTablas(){
-    let contenedor=document.getElementById("contenedor-tabla");
+function generarTablas() {
+    let contenedor = document.getElementById("contenedor-tabla");
+    
+    let factor = document.getElementById("numero-tabla").value;
+
+    if (factor === "") {
+        alert("Por favor, ingresa un número primero.");
+        return;
+    }
 
     let contenido = "";
-
     for (let i = 1; i <= 10; i++) {
-        
         contenido = contenido + `
             <tr>
-                <td>5 x ${i}</td>
-                <td class="glow-cell">${5 * i}</td>
-            </tr>`;    
+                <td>${factor} x ${i}</td>
+                <td class="glow-cell">${factor * i}</td>
+            </tr>`;
     }
-    contenedor.innerHTML=contenido;
+
+    contenedor.innerHTML = contenido;
 }
