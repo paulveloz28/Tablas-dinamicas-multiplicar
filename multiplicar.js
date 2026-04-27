8c624a0d643e2d04c1270d9306f7560c138c9ae0
paulveloz28 <paulveloz28@gmail.com>
2026-04-27 17:50:04 -0500

    feat: style change in the result section

--- multiplicar.js
+++ multiplicar.js
@@ -1,20 +1,27 @@
 function generarTablas() {
     let contenedor = document.getElementById("contenedor-tabla");
-    
+    let titulo = document.getElementById("titulo-tabla");
     let factor = document.getElementById("numero-tabla").value;
 
     if (factor === "") {
-        alert("Por favor, ingresa un número primero.");
+        alert("Por favor, ingresa un número.");
         return;
     }
 
+    titulo.innerText = `TABLA DEL ${factor}`;
+
     let contenido = "";
-    for (let i = 1; i <= 10; i++) {
-        contenido = contenido + `
-            <tr>
-                <td>${factor} x ${i}</td>
-                <td class="glow-cell">${factor * i}</td>
-            </tr>`;
+
+    // El bucle avanza de 2 en 2 (i = 1, 3, 5, 7, 9)
+    for (let i = 1; i <= 9; i += 2) {
+        let impar = i;
+        let par = i + 1;
+
+        contenido += `
+            <div class="result-row">
+                <div class="capsule">${factor} x ${impar} = ${factor * impar}</div>
+                <div class="capsule">${factor} x ${par} = ${factor * par}</div>
+            </div>`;
     }
 
     contenedor.innerHTML = contenido;
